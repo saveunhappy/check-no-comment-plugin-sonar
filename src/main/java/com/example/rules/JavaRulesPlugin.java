ec0df503c2140456a92e320561429ea32ad4a8e9
houjiangtao <925147954@qq.com>
2025-04-25 16:40:36 +0800

    1.这里就是 see rule看到的代码提示

--- src/main/java/com/example/rules/JavaRulesPlugin.java
+++ src/main/java/com/example/rules/JavaRulesPlugin.java
@@ -25,9 +25,23 @@ public class JavaRulesPlugin implements Plugin {
             // 从类注解中加载规则定义
             repo.createRule(MethodCommentCheck.class.getAnnotation(org.sonar.check.Rule.class).key())
                     .setName("Java方法必须包含注释（Blocker级别）")
-                    .setHtmlDescription("所有Java方法必须包含Javadoc注释，否则触发Blocker级别问题。")
+                    .setHtmlDescription("<p>此规则强制要求所有Java方法必须包含Javadoc注释。</p>\n" +
+                            "<h2>不合规代码示例</h2>\n" +
+                            "<pre>\n" +
+                            "public void doSomething() {\n" +
+                            "    // 这里缺少Javadoc注释\n" +
+                            "}\n" +
+                            "</pre>\n" +
+                            "<h2>合规代码示例</h2>\n" +
+                            "<pre>\n" +
+                            "/**\n" +
+                            " * 这个方法用于执行某些操作\n" +
+                            " */\n" +
+                            "public void doSomething() {\n" +
+                            "    // 方法实现\n" +
+                            "}\n" +
+                            "</pre>\n")
                     .setSeverity("BLOCKER");
-
             repo.done();
         }
     }
